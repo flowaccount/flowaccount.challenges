05a1b2bb00f4bd3baf7bf5b159f77a90c4058e34
Pichayawat Karnjanaves <pichayawat_k@flowaccount.com>
2021-03-23 11:55:46 +0700

    update transaction model.

--- android/app/src/main/java/com/android/pouch/entity/Transaction.kt
+++ android/app/src/main/java/com/android/pouch/entity/Transaction.kt
@@ -1,7 +1,12 @@
 package com.android.pouch.entity
 
 import com.google.gson.annotations.SerializedName
+import java.util.*
 
 data class Transaction(
-    @SerializedName("amount") var amount: Double
+        @SerializedName("id") var id: Long,
+        @SerializedName("type") var type: TransactionType,
+        @SerializedName("category") var category: String,
+        @SerializedName("amount") var amount: Double,
+        @SerializedName("createdAt") var createdAt: Calendar
 )
