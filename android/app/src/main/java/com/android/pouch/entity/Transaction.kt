03c71fd8d4c19ecabe7e7b18d351e8821e476070
Pichayawat Karnjanaves <pichayawat_k@flowaccount.com>
2021-03-23 11:56:52 +0700

    simplify transaction model.

--- android/app/src/main/java/com/android/pouch/entity/Transaction.kt
+++ android/app/src/main/java/com/android/pouch/entity/Transaction.kt
@@ -1,12 +1,11 @@
 package com.android.pouch.entity
 
 import com.google.gson.annotations.SerializedName
-import java.util.*
 
 data class Transaction(
         @SerializedName("id") var id: Long,
-        @SerializedName("type") var type: TransactionType,
+        @SerializedName("type") var type: Int,
         @SerializedName("category") var category: String,
         @SerializedName("amount") var amount: Double,
-        @SerializedName("createdAt") var createdAt: Calendar
+        @SerializedName("createdAt") var createdAt: String
 )
