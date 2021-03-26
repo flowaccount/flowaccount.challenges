5cea6a74112f831f757a7c59b76e19473f28e56d
Pongpanich Raksa <pongpanich_r@flowaccount.com>
2021-03-26 18:47:39 +0700

    update code

--- iOS/Pouch/View/Transaction/Controller/TransactionListController.swift
+++ iOS/Pouch/View/Transaction/Controller/TransactionListController.swift
@@ -10,10 +10,38 @@ import UIKit
 
 class TransactionListController: BaseViewController {
     
+    struct RawCell {
+        static let summaryCell = "TransactionSummaryCell"
+        static let detailCell = "TransactionDetailCell"
+    }
+    
     @IBOutlet var coll: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.registerCell()
     }
     
+    private func registerCell() {
+        coll.register(UINib(nibName: RawCell.summaryCell, bundle: nil), forCellWithReuseIdentifier: RawCell.summaryCell)
+        coll.register(UINib(nibName: RawCell.detailCell, bundle: nil), forCellWithReuseIdentifier: RawCell.detailCell)
+        coll.delegate = self
+        coll.dataSource = self
+    }
+    
+}
+
+extension TransactionListController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RawCell.summaryCell, for: indexPath) as! TransactionSummaryCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 120)
+    }
 }
