ff5e3a85e9f2331f2dd58abd4a9323a077120494
Narong <narong@flowaccount.com>
2021-04-02 15:38:41 +0700

    Update TransactionViewModel

--- iOS/Pouch/Extension/DI/DIViewModelExt.swift
+++ iOS/Pouch/Extension/DI/DIViewModelExt.swift
@@ -11,7 +11,7 @@ import Swinject
 extension Container {
     func registerViewModel() {
         self.register(TransactionViewModel.self) { (r) in
-            return TransactionViewModel(apiService: r.resolve(TransactionAPI.self)!, rx: r.resolve(RXPublishSubjectManager<[Transaction]>.self)!)
+            return TransactionViewModel(model: r.resolve(Transaction.self)!, apiService: r.resolve(TransactionAPI.self)!, rx: r.resolve(RXPublishSubjectManager<[TransactionViewModel]>.self)!)
         }
         self.register(SummaryViewModel.self) { (r) in
             return SummaryViewModel(model: r.resolve(Summary.self)!, apiService: r.resolve(TransactionAPI.self)!)
