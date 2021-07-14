f5e99ba387cec4ff20eefa7af3ef82bfc5259cfb
dotnetto <dotnetto@gmail.com>
2021-07-14 23:17:42 +0700

    fix connection to database

--- dotnet-developer/pouch/Startup.cs
+++ dotnet-developer/pouch/Startup.cs
@@ -33,10 +33,7 @@ namespace api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-
             services.AddControllers();
-         
                
             services.AddSwaggerGen(c =>
             {
